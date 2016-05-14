39f5b5b063ccc125895d4431fc22505d49149fa0
realgvard <realgvard@gmail.com>
2016-05-15 01:43:21 +0300

    add new files

---
+++ assets/js/loader.js
@@ -0,0 +1,13 @@
+
+
+// EXAMPLE 1
+
+var slider = $('.fullSizeSlider').deviora({
+    fullScreenOffsetY: $('.ui-header-block'),
+    devBeforeInit: function() {
+        $('.fullSizeSlider').css('opacity', 0);
+    },
+    devAfterInit: function() {
+        $('.fullSizeSlider').animate({ 'opacity': 1 }, 1200);
+    }
+});
